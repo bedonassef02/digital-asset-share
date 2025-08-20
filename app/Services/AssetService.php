64559a0c676fe4b3ae49043ab6e76f8f780b51d3
Bedo <bedonassef71@gmail.com>
2025-08-20 22:10:07 +0300

    refactor: Break down AssetService::create into smaller methods

--- app/Services/AssetService.php
+++ app/Services/AssetService.php
@@ -23,39 +23,53 @@ class AssetService
         return Asset::findOrFail($id);
     }
 
-    public function create(UploadedFile $file, string $disk, array $data = [])
+    public function create(UploadedFile $file, string $disk, array $data = []): Asset
     {
-        $assetData = array_merge($data, [
+        $assetData = $this->prepareAssetData($file, $disk, $data);
+        $asset = Asset::create($assetData);
+
+        $this->storeFile($file, $asset, $disk);
+        $this->generateThumbnail($file, $asset, $disk);
+
+        $asset->save();
+
+        return $asset;
+    }
+
+    private function prepareAssetData(UploadedFile $file, string $disk, array $data): array
+    {
+        return array_merge($data, [
             'file_name' => $file->getClientOriginalName(),
             'mime_type' => $file->getMimeType(),
             'size' => $file->getSize(),
             'disk' => $disk,
-            'extension' => $storedAsset['extension'],
-            'metadata' => json_encode([
-                'original_name' => $file->getClientOriginalName(),
-            ]),
+            'extension' => $file->getClientOriginalExtension(),
+            'metadata' => json_encode([]),
         ]);
+    }
 
-        $asset = Asset::create($assetData);
-
+    private function storeFile(UploadedFile $file, Asset $asset, string $disk): void
+    {
         $storedAsset = ($this->assetStorageService)($file, $asset->id, $disk);
 
         $asset->path = $storedAsset['path'];
         $asset->url = $storedAsset['url'];
+        $asset->extension = $storedAsset['extension'];
 
         $metadata = json_decode($asset->metadata, true);
         $metadata['hash_name'] = $storedAsset['hash_name'];
+        $asset->metadata = json_encode($metadata);
+    }
 
+    private function generateThumbnail(UploadedFile $file, Asset $asset, string $disk): void
+    {
         $thumbnailUrl = ($this->thumbnailService)($file, $asset->id, $disk);
 
         if ($thumbnailUrl) {
+            $metadata = json_decode($asset->metadata, true);
             $metadata['thumbnail_url'] = $thumbnailUrl;
+            $asset->metadata = json_encode($metadata);
         }
-
-        $asset->metadata = json_encode($metadata);
-        $asset->save();
-
-        return $asset;
     }
 
     public function update(int $id, array $data)
