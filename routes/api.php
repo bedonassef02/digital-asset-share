e554218f692f8686fe67166f52fa98bbd015c839
Bedo <bedonassef71@gmail.com>
2025-08-21 17:19:36 +0300

    feat: Implement API authentication with Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AssetController;
 use App\Http\Controllers\ServeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/test', function (Request $request) {
     return response()->json(['message' => 'API is working']);
@@ -12,3 +13,12 @@ Route::get('/test', function (Request $request) {
 Route::apiResource('/assets', AssetController::class);
 
 Route::get('/serve/{id}', ServeController::class);
+
+// Authentication routes
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/user', [App\Http\Controllers\Api\AuthController::class, 'user']);
+});
