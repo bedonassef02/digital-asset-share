ff51b5a41ee9b7ff827b1876c98df8d7c07822cb
Bedo <bedonassef71@gmail.com>
2025-08-21 14:43:00 +0300

    refactor: Standardize asset metadata structure

--- app/Services/MediaProcessors/AudioProcessor.php
+++ app/Services/MediaProcessors/AudioProcessor.php
@@ -33,12 +33,7 @@ class AudioProcessor implements MediaProcessorInterface
         $fileInfo = $getID3->analyze($file->getPathname());
 
         // Populate metadata array with relevant audio information
-        $metadata['media_type'] = 'audio';
-        $metadata['audio_info'] = [
-            'mime_type' => $file->getMimeType(),
-            'original_name' => $file->getClientOriginalName(),
-            'size' => $file->getSize(),
-            'format' => $fileInfo['fileformat'] ?? null,
+        $metadata = [
             'encoding' => $fileInfo['encoding'] ?? null,
             'bitrate' => $fileInfo['audio']['bitrate'] ?? null,
             'sample_rate' => $fileInfo['audio']['sample_rate'] ?? null,
@@ -46,14 +41,9 @@ class AudioProcessor implements MediaProcessorInterface
             'duration_seconds' => $fileInfo['playtime_seconds'] ?? null,
             'duration_string' => $fileInfo['playtime_string'] ?? null,
             'artist' => $fileInfo['tags']['id3v2']['artist'][0] ?? ($fileInfo['tags']['id3v1']['artist'][0] ?? null),
-            'title' => $fileInfo['tags']['id3v2']['title'][0] ?? ($fileInfo['tags']['id3v1']['title'][0] ?? null),
             'album' => $fileInfo['tags']['id3v2']['album'][0] ?? ($fileInfo['tags']['id3v1']['album'][0] ?? null),
             'year' => $fileInfo['tags']['id3v2']['year'][0] ?? ($fileInfo['tags']['id3v1']['year'][0] ?? null),
             'genre' => $fileInfo['tags']['id3v2']['genre'][0] ?? ($fileInfo['tags']['id3v1']['genre'][0] ?? null),
         ];
-
-        // Add duration directly to the top-level metadata for easier access/storage
-        $metadata['duration_seconds'] = $fileInfo['playtime_seconds'] ?? null;
-        $metadata['duration_string'] = $fileInfo['playtime_string'] ?? null;
     }
 }
