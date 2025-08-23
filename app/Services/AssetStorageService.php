a165c5db8a8afccd40472f565d544041e38ae10b
Bedo <bedonassef71@gmail.com>
2025-08-23 11:55:00 +0300

    feat(storage): implement sharding for asset storage

--- app/Services/AssetStorageService.php
+++ app/Services/AssetStorageService.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Storage;
 
 class AssetStorageService
 {
+    private const SHARD_SIZE = 1000;
+
     public function __invoke(UploadedFile $file, int $assetId, int $version): string
     {
-        $assetDirectory = 'uploads/' . $assetId . '/' . $version;
+        $assetDirectory = $this->getAssetVersionPath($assetId, $version);
         Storage::disk()->makeDirectory($assetDirectory);
 
         return Storage::disk()->putFileAs($assetDirectory, $file, 'file');
@@ -17,13 +19,29 @@ class AssetStorageService
 
     public function deleteDirectory(int $assetId): bool
     {
-        $assetDirectory = 'uploads/' . $assetId;
+        $assetDirectory = $this->getAssetPath($assetId);
         return Storage::disk()->deleteDirectory($assetDirectory);
     }
 
     public function deleteVersion(int $assetId, int $version): bool
     {
-        $assetDirectory = 'uploads/' . $assetId . '/' . $version;
+        $assetDirectory = $this->getAssetVersionPath($assetId, $version);
         return Storage::disk()->deleteDirectory($assetDirectory);
+    }
+
+    private function getShardDirectory(int $assetId): string
+    {
+        $shardId = floor(($assetId - 1) / self::SHARD_SIZE);
+        return 'uploads/' . $shardId;
+    }
+
+    private function getAssetPath(int $assetId): string
+    {
+        return $this->getShardDirectory($assetId) . '/' . $assetId;
+    }
+
+    public function getAssetVersionPath(int $assetId, int $version): string
+    {
+        return $this->getAssetPath($assetId) . '/' . $version;
     }
 }
