92921beac39e84207cd123d5c40e8974fe8556fa
Bedo <bedonassef71@gmail.com>
2025-08-25 15:52:37 +0300

    feat: Implement database read/write splitting

--- config/database.php
+++ config/database.php
@@ -45,8 +45,17 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'read' => [
+                'host' => [
+                    env('DB_READ_HOST', '127.0.0.1'), // Can be an array of hosts for multiple replicas
+                ],
+                'port' => env('DB_READ_PORT', '3306'),
+            ],
+            'write' => [
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '3306'),
+            ],
+            'sticky' => true, // Optional: ensures subsequent reads after a write go to the write connection for the same request
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
