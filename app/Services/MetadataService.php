6203c859bc22f87abca323e82410d1ed309e532e
Bedo <bedonassef71@gmail.com>
2025-08-25 16:18:59 +0300

    refactor: Update MetadataService to use asset_versions metadata column

--- app/Services/MetadataService.php
+++ app/Services/MetadataService.php
@@ -11,56 +11,24 @@ class MetadataService
 {
     public function __construct(
         private MediaService $mediaService,
-        private StorageService $storageService
     ) {}
 
     public function __invoke(
         UploadedFile $file,
         AssetVersion $assetVersion,
     ): void {
+        $metadata = [];
         $metadata['hash'] = hash_file('sha256', $file->getRealPath());
 
         $this->mediaService->setProperties($file, $metadata);
 
-        $this->store($assetVersion, $metadata);
-    }
-
-    public function store(AssetVersion $assetVersion, array $metadata): string
-    {
-        $metadataFilePath = $this->getMetadataFilePath($assetVersion);
-
-        return Storage::disk()->put($metadataFilePath, json_encode($metadata, JSON_PRETTY_PRINT));
+        $assetVersion->update(['metadata' => $metadata]);
     }
 
     public function update(AssetVersion $assetVersion, array $data): void
     {
-        $metadataFilePath = $this->getMetadataFilePath($assetVersion);
-
-        if (Storage::disk()->exists($metadataFilePath)) {
-            $metadata = json_decode(Storage::disk()->get($metadataFilePath), true);
-            $updatedMetadata = array_merge($metadata, $data);
-            $this->store($assetVersion, $updatedMetadata);
-        }
-    }
-
-    public function get(Asset $asset): array
-    {
-        if (!$asset->latestVersion) {
-            return [];
-        }
-
-        $metadataFilePath = $this->storageService->pathService->getAssetVersionPath($asset->id, $asset->latestVersion->version) . '/metadata.json';
-
-        if (Storage::disk()->exists($metadataFilePath)) {
-            $metadataContent = Storage::disk()->get($metadataFilePath);
-            return json_decode($metadataContent, true);
-        }
-
-        return [];
-    }
-
-    private function getMetadataFilePath(AssetVersion $assetVersion): string
-    {
-        return $this->storageService->pathService->getAssetVersionPath($assetVersion->asset_id, $assetVersion->version) . '/metadata.json';
+        $currentMetadata = $assetVersion->metadata ?? [];
+        $updatedMetadata = array_merge($currentMetadata, $data);
+        $assetVersion->update(['metadata' => $updatedMetadata]);
     }
 }
