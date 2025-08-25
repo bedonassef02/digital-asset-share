d6cedb9c1b91dcfb75836a11e3cee89ac6c82150
Bedo <bedonassef71@gmail.com>
2025-08-25 16:19:03 +0300

    refactor: Update AssetResource to use direct metadata access

--- app/Http/Resources/AssetResource.php
+++ app/Http/Resources/AssetResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Services\MetadataService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,13 +14,11 @@ class AssetResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $metadataService = app(MetadataService::class);
-
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
             'latest_version' => $this->whenLoaded('latestVersion'),
-            'metadata' => $metadataService->get($this->resource),
+            'metadata' => $this->latestVersion->metadata ?? [],
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
